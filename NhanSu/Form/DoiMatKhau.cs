a3c1cdeadefe94b2bd807142744f95aa96ed8979
naq259 <naq259@gmail.com>
2017-04-27 08:24:05 +0700

    NhanVien

---
+++ NhanSu/Form/DoiMatKhau.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace NhanSu
+{
+    public partial class DoiMatKhau : Form
+    {
+        public DoiMatKhau()
+        {
+            InitializeComponent();
+        }
+
+        private void btXacNhan_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btNhapLai_Click(object sender, EventArgs e)
+        {
+            tbTenDangNhap.Text = "";
+            tbMatKhauCu.Text = "";
+            tbMatKhauMoi.Text="";
+            bXacNhanMK.Text="";
+        }
+    }
+}
---
+++ NhanSu/Form/MatKhau.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace NhanSu
+{
+    public partial class MatKhau : Form
+    {
+      //  private NguoiDungBLL nguoidung;
+      //  NguoiDungEntities nguoidungenti;
+        public MatKhau()
+        {
+            InitializeComponent();
+            //nguoidung = new NguoiDungBLL();
+        }
+
+        private void btDangNhap_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btDoiMK_Click(object sender, EventArgs e)
+        {
+            DoiMatKhau formdoimk = new DoiMatKhau();
+            formdoimk.Show();
+        }
+    }
+}
